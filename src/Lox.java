6958c7dd1a9d6d0091480edbe3c4124c3d6cef27
Josh <josh.e.stein@gmail.com>
2024-04-08 16:46:52 -0500

    Init

---
+++ src/Lox.java
@@ -0,0 +1,12 @@
+public class Lox {
+    public static void main(String[] args) {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+}
